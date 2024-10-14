9779c004b89e56c393eab679f23729e3802c2b32
rmdhniki <rmdhniki18@gmail.com>
2024-10-14 11:10:51 +0700

    Mengerjakan Pertemuan 4

---
+++ Pertemuan4/Latihan3/KeyListenerExample.java
@@ -0,0 +1,49 @@
+package Pertemuan4.Latihan3;
+
+import javax.swing.JLabel;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class KeyListenerExample {
+    public static void main(String [] args) {
+        //Membuat frame
+        JFrame frame = new JFrame("Keylistener Example");
+
+        // Membuat label untuk menampilkan pesan
+        JLabel label = new JLabel("Tekan tombol pada keyboard.");
+        label.setBounds(50, 100, 200, 30);
+
+        //Membuat text field untuk fokus keyboard
+        JTextField textField = new JTextField();
+        textField.setBounds(50, 100, 200, 30);
+
+        //membuat keylintener ke text field
+        textField.addKeyListener(new KeyListener() {
+            // dijalankan ketika tombol ditekan
+            public void keyPressed(KeyEvent e){
+                label.setText("Key Pressed:" + KeyEvent.getKeyText(e.getKeyCode()));
+            }
+        //dijalankan ketika tombol dilepaskan
+        public void keyReleased(KeyEvent e) {
+            label.setText("Key Pressed:" + KeyEvent.getKeyText(e.getKeyCode()));
+        }
+        //dijalankan ketika tombol ditekan dan dilepaskan (sama dengan mengetik karakter)
+        public void keyTyped(KeyEvent e) {
+            label.setText("Key Typed: " + e.getKeyChar());
+        }
+    });
+
+    //Menambahkan komponen ke frame
+    frame.add(label);
+    frame.add(textField);
+
+    //Setting frame
+    frame.setSize(400, 200);
+    frame.setLayout(null);
+    frame.setVisible(true);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+    }
+}
